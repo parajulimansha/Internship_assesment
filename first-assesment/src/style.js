cbd04d12f2f2aa8b6e66a79db2fa8b0153eb7ebc
parajulimansha <parajulimansha@gmail.com>
2024-08-29 22:36:31 +0545

    minor changes

---
+++ first-assesment/src/style.js
@@ -0,0 +1,76 @@
+// set the dropdown menu element
+const $targetEl = document.getElementById('dropdownMenu');
+
+// set the element that trigger the dropdown menu on click
+const $triggerEl = document.getElementById('dropdownButton');
+
+// options with default values
+const options = {
+    placement: 'bottom',
+    triggerType: 'click',
+    offsetSkidding: 0,
+    offsetDistance: 10,
+    delay: 300,
+    ignoreClickOutsideClass: false,
+    onHide: () => {
+        console.log('dropdown has been hidden');
+    },
+    onShow: () => {
+        console.log('dropdown has been shown');
+    },
+    onToggle: () => {
+        console.log('dropdown has been toggled');
+    },
+};
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const slides = document.getElementById('carouselSlides');
+  const prevBtn = document.getElementById('prevBtn');
+  const nextBtn = document.getElementById('nextBtn');
+  const slideCount = slides.children.length;
+  let currentSlide = 0;
+
+  function updateSlidePosition() {
+    const offset = -currentSlide * 100; // Move the slides
+    slides.style.transform = `translateX(${offset}%)`;
+  }
+
+  prevBtn.addEventListener('click', function () {
+    currentSlide = (currentSlide > 0) ? currentSlide - 1 : slideCount - 1;
+    updateSlidePosition();
+  });
+
+  nextBtn.addEventListener('click', function () {
+    currentSlide = (currentSlide < slideCount - 1) ? currentSlide + 1 : 0;
+    updateSlidePosition();
+  });
+
+  
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const counters = document.querySelectorAll('.counter');
+    const speed = 200; // Adjust the speed as needed
+
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+
+            // Lower increment to slow and higher increment to speed up
+            const increment = target / speed;
+
+            if (count < target) {
+                counter.innerText = Math.ceil(count + increment);
+                setTimeout(updateCount, 1);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCount();
+    });
+});
+
+
